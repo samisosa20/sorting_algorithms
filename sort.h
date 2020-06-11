b803cea8d193cca73ad2e94b7bd3591333219c11
samisosa20 <1449@holbertonschool.com>
2020-06-11 16:20:28 +0000

    task 0

--- sort.h
+++ sort.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -30,4 +31,3 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
